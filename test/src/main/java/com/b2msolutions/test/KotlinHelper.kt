f888c1c8f5df54a7541865e3bef16a8b36331316
Pavel Luzhetskiy <supoved@users.noreply.github.com>
2020-07-10 16:10:30 +0100

    Added kotlin helper class to support mockito any matchers (#6)

---
+++ test/src/main/java/com/b2msolutions/test/KotlinHelper.kt
@@ -0,0 +1,34 @@
+package com.b2msolutions.test
+
+import org.mockito.Mockito
+
+abstract class KotlinHelper {
+    companion object {
+        fun setStaticField(item: Any, propName: String, value: Any?) {
+            val field = item::class.java.getDeclaredField(propName)
+            field.isAccessible = true
+            field.set(item, value)
+        }
+
+        fun getStaticField(item: Any, propName: String): Any {
+            val field = item::class.java.getDeclaredField(propName)
+            field.isAccessible = true
+            return field.get(item)
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        fun <T> uninitialized(): T = null as T
+
+        fun <T> eq(value: T): T {
+            return Mockito.eq(value) ?: uninitialized()
+        }
+
+        fun <T> any(type: Class<T>): T {
+            return Mockito.any(type) ?: uninitialized()
+        }
+
+        fun <T> anyObject(): T {
+            return Mockito.any<T>() ?: uninitialized()
+        }
+    }
+}
